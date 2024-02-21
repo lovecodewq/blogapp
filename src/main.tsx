2ee0dc35e9e386bd77d26c7ba8dd1f1402b18eba
Wenqiang Li <wenqiangli@autox.ai>
2024-02-21 10:06:46 +0800

    add react brower router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowerRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowerRouter([]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
