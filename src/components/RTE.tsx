7fbd3e4156516a1a5d5ef9f864f27d7f7ed286ae
Wenqiang Li <wenqiangli@autox.ai>
2024-02-23 11:45:57 +0800

    add controller

---
+++ src/components/RTE.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Controller, Control } from 'react-hook-form'
+import { Editor } from '@tinymce/tinymce-react'
+
+interface RTEProps {
+  name: string
+  control: Control
+  label: string
+  defaultValue: string
+}
+
+function RTE({ name, control, label, defaultValue = '' }: RTEProps) {
+  return (
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+      <Controller
+        name={name || 'content'}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            onEditorChange={onChange}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                'advlist',
+                'autolink',
+                'lists',
+                'link',
+                'image',
+                'charmap',
+                'preview',
+                'anchor',
+                'searchreplace',
+                'visualblocks',
+                'code',
+                'fullscreen',
+                'insertdatetime',
+                'media',
+                'table',
+                'code',
+                'help',
+                'wordcount',
+              ],
+              toolbar:
+                'undo redo | blocks | ' +
+                'bold italic forecolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+              content_style:
+                'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+            }}
+          />
+        )}
+      />
+    </div>
+  )
+}
+
+export default RTE
