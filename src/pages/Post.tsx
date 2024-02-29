e13fbf05a322c903fcf4c99bd92fd1a0c9d862cb
Wenqiang Li <wenqiangli@autox.ai>
2024-02-29 17:29:53 +0800

    add post

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import service from '../appwrite/service'
+import Button from '../components/Button'
+import Container from '../components/container/Container'
+import parse from 'html-react-parser'
+import { useSelector } from 'react-redux'
+import type { RootState } from '../store/store'
 
 function Post() {
-  return (
-    <div>Post</div>
-  )
+  const [post, setPost] = useState(null)
+  const { slug } = useParams()
+  const navigate = useNavigate()
+  const userData = useSelector((state: RootState) => state.auth.userData)
+  const isAuthor = post && userData ? post.userId === userData.$id : false
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      if (slug) {
+        try {
+          const post = await service.getDocument(slug)
+          if (post) {
+            setPost(post)
+          } else {
+            navigate('/')
+          }
+        } catch (error) {
+          console.log('Failed to fetch post: ', error)
+          navigate('/')
+        }
+      }
+    }
+    fetchPost()
+  }, [slug, navigate])
+
+  const deletePost = async () => {
+    if (post && post.$id) {
+      try {
+        const status = await service.deleteDocument(post.$id)
+        if (status) {
+          service.deleteFile(post.featuredImage)
+          navigate('/')
+        }
+      } catch (error) {
+        console.log('Failed to delete post: ', error)
+        navigate('/')
+      }
+    }
+  }
+
+  return post ? (
+    <div className='py-8'>
+      <Container>
+        <div className='w-full flex justify-center mb-4 relative border rounded-xl p-2'>
+          <img
+            src={service.getFilePreview(post.featuredImage)}
+            alt={post.title}
+            className='rounded-xl'
+          />
+          {isAuthor && (
+            <div>
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor='bg-green-500' className='mr-3'>
+                  Edit
+                </Button>
+              </Link>
+              <Button bgColor='bg-red-500' onClick={deletePost}>
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className='w-full mb=6'>
+          <h1 className='text-2xl font-bold'>{post.title}</h1>
+          <div className='browser-css'>{parse(post.content)}</div>
+        </div>
+      </Container>
+    </div>
+  ) : null
 }
+
+export default Post
 
 export default Post
