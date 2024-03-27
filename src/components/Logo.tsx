1ce62bbe39eb67c61997f4eded1048da21dfbefd
Wenqiang Li <wenqiangli@WenqiangdeMacBook-Pro.local>
2024-03-27 18:31:51 +0800

    add logo picture

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import logo from '../assets/blog_logo.webp'
 
 interface LogoProps {
   width?: string
   maxHeight?: string
 }
-function Logo({ width = '100%', maxHeight = '100px' }: LogoProps) {
+function Logo({ width = '50%', maxHeight = '100px' }: LogoProps) {
   return (
     <img
-      className='rounded-xl'
-      src='https://images.pexels.com/photos/3077882/pexels-photo-3077882.jpeg?auto=compress&cs=tinysrgb&w=600'
+      className='rounded-full'
+      src={logo}
       style={{ width, maxHeight, background: 'transparent' }}
       alt='Logo placeholder'
     />
